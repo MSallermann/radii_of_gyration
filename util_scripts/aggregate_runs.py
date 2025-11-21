b2d2616e199a68deac4bf6bb42af0a2830eece46
Moritz Sallermann <moritzsallermann@gmail.com>
2025-11-21 11:52:17 +0100

    util_scripts: better aggregate_runs script

--- util_scripts/aggregate_runs.py
+++ util_scripts/aggregate_runs.py
@@ -6,18 +6,24 @@ script_folder = Path(__file__).parent.parent / "workflow" / "scripts"
 sys.path.insert(0, script_folder.as_posix())
 import aggregate_json  # type: ignore # noqa: E402
 
-RUN_FOLDER = Path("/gpfs/projects/ucm96/moritz/rg_workflow/runs/test100")
-with (RUN_FOLDER / "samples.json").open("rb") as f:
-    SAMPLES = json.load(f)
 
-aggregate_json.main(
-    input_paths=[RUN_FOLDER / f"results/rg/{sample}/rg.json" for sample in SAMPLES],
-    output_path=RUN_FOLDER / "aggregated.csv",
-    added_columns={
-        "accession": [SAMPLES[sample]["accession"] for sample in SAMPLES],
-        "threshold": [SAMPLES[sample]["threshold"] for sample in SAMPLES],
-        "temperature": [SAMPLES[sample]["temperature"] for sample in SAMPLES],
-        "ionic_strength": [SAMPLES[sample]["ionic_strength"] for sample in SAMPLES],
-    },
-    ignore_keys=["file"],
-)
+def aggregate_from_run_folder(run_folder: Path):
+    with (run_folder / "samples.json").open("rb") as f:
+        samples = json.load(f)
+
+    aggregate_json.main(
+        input_paths=[run_folder / f"results/rg/{sample}/rg.json" for sample in samples],
+        output_path=run_folder / "aggregated.csv",
+        added_columns={
+            "accession": [samples[sample]["accession"] for sample in samples],
+            "threshold": [samples[sample]["threshold"] for sample in samples],
+            "temperature": [samples[sample]["temperature"] for sample in samples],
+            "ionic_strength": [samples[sample]["ionic_strength"] for sample in samples],
+        },
+        ignore_keys=["file"],
+    )
+
+
+if __name__ == "__main__":
+    run_folder = Path("/gpfs/projects/ucm96/moritz/rg_workflow/runs/test100")
+    aggregate_from_run_folder(run_folder)
