fc4e2be79e0ab240750048a3f2e92d4c149d4ae1
Moritz Sallermann <moritzsallermann@gmail.com>
2026-02-11 12:15:07 +0100

    analyse_lammps_log: analyse density as well

--- workflow/scripts/analyse_lammps_log.py
+++ workflow/scripts/analyse_lammps_log.py
@@ -14,7 +14,7 @@ def analyse_lammps_log(log_file: Path, n_skip: int, output_json: Path):
     # This example uses the 'crack_log.lammps' file found in 'examples/logfiles/'
     lf = File(log_file)
 
-    cols = ["PotEng", "Press", "KinEng", "Temp"]
+    cols = ["PotEng", "Press", "KinEng", "Temp", "Density"]
 
     avg_data = {}
 
@@ -23,37 +23,41 @@ def analyse_lammps_log(log_file: Path, n_skip: int, output_json: Path):
     for col in cols:
         fig, ax = plt.subplots()
 
-        col_vals = lf.get(col)
+        try:
+            col_vals = lf.get(col)
 
-        avg_data[col] = np.mean(col_vals[n_skip:])
+            avg_data[col] = np.mean(col_vals[n_skip:])
 
-        ax.plot(
-            steps[:n_skip],
-            col_vals[:n_skip],
-            color="grey",
-            marker=".",
-            ls="None",
-            label="skipped",
-        )
+            ax.plot(
+                steps[:n_skip],
+                col_vals[:n_skip],
+                color="grey",
+                marker=".",
+                ls="None",
+                label="skipped",
+            )
 
-        ax.plot(
-            steps[n_skip:],
-            col_vals[n_skip:],
-            color="blue",
-            marker=".",
-            ls="None",
-            label="computed",
-        )
+            ax.plot(
+                steps[n_skip:],
+                col_vals[n_skip:],
+                color="blue",
+                marker=".",
+                ls="None",
+                label="computed",
+            )
 
-        ax.plot(steps, moving_avg(col_vals), label="running mean", color="black")
+            ax.plot(steps, moving_avg(col_vals), label="running mean", color="black")
 
-        ax.axhline(avg_data[col], ls="--", color="black", label="total mean")
+            ax.axhline(avg_data[col], ls="--", color="black", label="total mean")
 
-        ax.legend()
-        ax.set_xlabel("Step")
-        ax.set_ylabel(col)
+            ax.legend()
+            ax.set_xlabel("Step")
+            ax.set_ylabel(col)
 
-        fig.savefig(output_json.parent / f"{col}.png", dpi=300)
+            fig.savefig(output_json.parent / f"{col}.png", dpi=300)
+
+        except Exception as e:
+            print(e)
 
     with open(output_json, "w") as f:
         json.dump(avg_data, f, indent=4)
